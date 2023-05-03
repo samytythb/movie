e91bb174856a8499dc626d9e075c670cb90efe19
Le Minh Duc <79952214+samytythb@users.noreply.github.com>
2023-05-03 20:28:13 +0700

    update

---
+++ src/api/axiosConfig.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+export default axios.create({
+  baseURL: "https://api-movie-java-production.up.railway.app",
+});
---
+++ src/components/home/Home.js
@@ -0,0 +1,5 @@
+import Hero from "../hero/Hero";
+const Home = ({ movies }) => {
+  return <Hero movies={movies} />;
+};
+export default Home;
